1d0056eaa01ff668a4f679a57f91c4351f064177
mattwacs <mwoodington@aircompressorservices.com>
2024-07-19 08:27:41 -0400

    height 600

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       spacing: {
-        900: "900px",
+        600: "600px",
       }
     },
   },
